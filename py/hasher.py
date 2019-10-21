35e961b82c38fecc01e4852db1858731b9e5dca5
the-sides <sides798@gmail.com>
2019-10-20 22:29:53 -0400

    Early race runs

--- py/hasher.py
+++ py/hasher.py
@@ -1,10 +1,11 @@
 from Crypto.Hash import SHA256 as sha
+from math import ceil
 
 def compByte(b1, b2):
     # Comparing the size of the length of binary bits will already determine the 8-L amount of bits.
+    xor = b1 ^ b2
     print('comparing:  ', b1, b2)
     print('binary form: {}({}) {}({})'.format(bin(b1), len(bin(b1))-2, bin(b2), len(bin(b2))-2))
-    xor = b1 ^ b2
     print('XOR output: ', bin(xor))
 
     # bin(byte) will produce the string output of a bits in the notation '0b00110011' 
@@ -18,7 +19,10 @@ def compByte(b1, b2):
 
     if b1N == b2N:
         # Xor starts where bits differ, so look at the length of the xor.
-        bits = 8 - int(len(bin(xor))-2)
+        if xor == 0: 
+            bits = 8
+        else:
+            bits = 8 - int(len(bin(xor))-2)
 
     print('leading bits similar: {}'.format(bits))
 
@@ -27,58 +31,68 @@ def compByte(b1, b2):
 # Will compare last hash with all those before
 def compHash(hash1, hash2):
     bits = 0
-    for char1 in hash1:
-        for char2 in hash2:
-            if char1 != char2:
-                break
-            else:
-                bits += 1
+    for i, char1 in enumerate(hash1):
+        bits += compByte(char1, hash2[i])
+        if (bits % 8 != 0) | (bits == 0):
+            print("bits collided: {}".format(bits))
+            break
+        else:
+            print("STILL GOING!")
+
     return bits
 
     
-H = sha.new()
-H.update(b'sideshawkin')
-h0 = H.digest()
-print(h0[0])
-for g in range(len(h0)-1):
-    compByte(h0[g], h0[g+1])
+def debugCompByte():
+    H = sha.new()
+    H.update(b'sideshawkin')
+    h0 = H.digest()
+    print(h0[0])
+    for g in range(len(h0)-1):
+        compByte(h0[g], h0[g+1])
     
-
-hashes = []
-
-hashCount = 50
-print('Hashing {} times', hashCount)
-
-prev = b'sideshawkin'
-for i in range(hashCount):
-    H = sha.new(prev)
-    prev = H.digest()
-    hashes.append(prev)
-    
-
-print("All hashes computed")
-print("Time to access hashes")
-
-for i in range(hashCount):
-    hashes[i]
 
 # Once a handful of hashes have been calculated, this is the race that will compare hash outcomes
 def startRace(h):
+    collisions = []
     tortInd = 0
     hareInd = 1
     tort = h[tortInd]
     hare = h[hareInd]
-    for i in range(len(h)):
+    for i in range(int(len(h)/2)):
 
         tortInd += 1
-        hareInd = 2
+        hareInd += 2
         tort = h[tortInd]
         hare = h[hareInd]
         if compHash(tort, hare) >= 16:
+            collisions.append(h[tortInd-1])
+            collisions.append(h[hareInd-1])
             print("Hash collision of 16 MSB bits")
             break
+            
+    return collisions
 
 
+hashes = []
 
+hashCount = 2000000
+bitTarget = 16
+byteTarget = ceil(bitTarget/8)
+print('Hashing {} times', hashCount)
+print('byteTarget {}', byteTarget)
 
+prev = b'sideshawkin'
+for i in range(hashCount):
+    H = sha.new(prev)
+    prev = H.digest()[:byteTarget]
+    hashes.append(prev)
+    
+
+print("{} hashes computed".format(hashCount))
+print("Looking for collisions...")
+
+collides = startRace(hashes)
+
+print("Finished searching for collisions...")
+print("Found: ", collides)
 
