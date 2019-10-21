a73d510e4614b45c4b8094a60e021904432cd0be
the-sides <sides798@gmail.com>
2019-10-20 21:36:34 -0400

    byte comparison alg working

--- py/hasher.py
+++ py/hasher.py
@@ -3,21 +3,26 @@ from Crypto.Hash import SHA256 as sha
 def compByte(b1, b2):
     # Comparing the size of the length of binary bits will already determine the 8-L amount of bits.
     print('comparing:  ', b1, b2)
-    print('binary form:', bin(b1), bin(b2))
+    print('binary form: {}({}) {}({})'.format(bin(b1), len(bin(b1))-2, bin(b2), len(bin(b2))-2))
     xor = b1 ^ b2
     print('XOR output: ', bin(xor))
-    bits = len(bin(b2)) - len(bin(xor))
 
     # bin(byte) will produce the string output of a bits in the notation '0b00110011' 
     #   so the first 2 chars unused. If 2 bytes share a smaller length, then the 
     #   first bits collide with the fact that they're both 0.
     b1N = len(bin(b1)) - 2
     b2N = len(bin(b2)) - 2
-    longest = b1N if b1N > b2N else b2N
-    shortest = b2N if b1N > b2N else b1N
-    print('leading bits similar: {}'.format(8 - longest - shortest))
-    # for i in range(8):
-    #     for j in range(8):
+    
+    # Bits matching (leading zeros if both aren't using all 8 bits)
+    bits = 8 - b1N if b1N > b2N else 8 - b2N
+
+    if b1N == b2N:
+        # Xor starts where bits differ, so look at the length of the xor.
+        bits = 8 - int(len(bin(xor))-2)
+
+    print('leading bits similar: {}'.format(bits))
+
+    return bits
 
 # Will compare last hash with all those before
 def compHash(hash1, hash2):
@@ -44,9 +49,12 @@ hashes = []
 hashCount = 50
 print('Hashing {} times', hashCount)
 
+prev = b'sideshawkin'
 for i in range(hashCount):
-    H = sha.new(b'deadbeef')
-    hashes.append(H.digest())
+    H = sha.new(prev)
+    prev = H.digest()
+    hashes.append(prev)
+    
 
 print("All hashes computed")
 print("Time to access hashes")
