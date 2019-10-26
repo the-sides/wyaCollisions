ee695a97c7d796312c9df2f81543aa625e939111
the-sides <sides798@gmail.com>
2019-10-26 18:12:15 -0400

    Shorter script for flat out racing and comparing

--- py/racing.py
+++ py/racing.py
@@ -1,24 +1,43 @@
 from Crypto.Hash import SHA256 as shaAlg
 
+bitsN = 16
+
 def sha(val):
     h = shaAlg.new(val)
-    return h.digest()
+    return h.digest()[:int(bitsN/8)]
 
-def race():
-    hashCount = 2000
+def race(hashCount = 20000000):
     tortInd = 0
     hare = 1
 
-    tort = sha(b'sideshawkin')
+    tortVal = b'sideshawkin1'
+    tort = sha(tortVal)
+
+    hareVal = tort
     hare = sha(tort)
 
+    count = 0
+    history = []
     for i in range(hashCount):
-        pretort = tort[0]
-        prehare = hare[0]
-        tort = sha(tort[:2])
-        hare = sha(sha(hare[:2]))
-        if tort[0] == hare[0]:
-            print("Found tort:{}     hare:{}")
-            print("From  tort:{}     hare:{}")
+        tortVal = tort
+        hareVal = sha(hare)
+        tort = sha(tortVal)
+        hare = sha(hareVal)
+
+        if tort == hare:
+            if "{}{}".format(tort, hare) in history:
+                continue
+            else:
+                history.append("{}{}".format(tort, hare))
+
+            if tortVal == hareVal: 
+                continue
+            count += 1
+
+
+            print("{}) Found tort:{}     hare:{}".format(count, tort, hare))
+            print("    From  val:{}         val:{}".format(tortVal, hareVal))
             
+if __name__ == '__main__':
+    race()
 
